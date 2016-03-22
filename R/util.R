e059bc6b6918396bc040f2fba65d9b51d1971549
Kevin Bensema <kbensema@ucdavis.edu>
2016-03-22 10:43:04 -0700

    Fixed an error in R/util.R

--- R/util.R
+++ R/util.R
@@ -11,10 +11,10 @@ OpenCVRectToRList <- function(opencv.rect)
   #   A named list with four elements: the argument rectangle's x, and y
   #   coordinates for it's lower-left corner, along with the width and height
   output <- list()
-  output$x = cvrect$x
-  output$y = cvrect$y
-  output$width = cvrect$width
-  output$height = cvrect$height
+  output$x = opencv.rect$x
+  output$y = opencv.rect$y
+  output$width = opencv.rect$width
+  output$height = opencv.rect$height
   return(output)
 }
 
